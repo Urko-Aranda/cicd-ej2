ea51850567b0749eca0e10fe6b9d804755d9d8f0
Urko-Aranda <urkoaragom@gmail.com>
2024-11-12 16:07:37 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+# Definición de la función que suma dos números
+def suma(a, b):
+    return a + b
+
+# Variables con los números elegidos
+num1 = 5
+num2 = 8
+
+# Comando para imprimir la suma de los dos números usando la función
+print("La suma de", num1, "y", num2, "es:", suma(num1, num2))
